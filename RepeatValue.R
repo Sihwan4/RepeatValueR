f75f79d4e6dd81f6ce24d9496d5be880cd1f2f13
Sihwan4 <53251312+Sihwan4@users.noreply.github.com>
2020-06-28 17:53:06 +0900

    Delete RepeatValue.R

--- RepeatValue.R
+++
@@ -1,6 +0,0 @@
-value <- "Apple"   # 반복할 값 지정 ("Apple", 1, 2, 3 등)
-length <- 50     # 반복 횟수를 지정
-write.table(matrix(rep(value,length),ncol=1),
-            file="clipboard",
-            row.names=FALSE,
-            col.names = FALSE)
